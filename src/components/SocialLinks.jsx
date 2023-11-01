ef2a7233981722fd3ec5df4f4e80c9fb74c2e1fa
rmmilsap <rmmilsap@crimson.ua.edu>
2023-10-31 22:22:53 -0500

    fixed sociallinks unused var

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaGithub, FaLink, FaLinkedin } from 'react-icons/fa'
+import { FaGithub, FaLinkedin } from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 
@@ -59,7 +59,7 @@ const SocialLinks = () => {
 
         <li 
         key={id} 
-        className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500' + ' ' + style}>
+        className={'flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 ' + style}>
           
           <a 
           href={href} 
